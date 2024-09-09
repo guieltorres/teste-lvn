0cf8c190974013e21e308201db0f500d2d94baeb
guieltorres <guieltorres@outlook.com>
2024-09-09 19:13:19 -0300

    fix: product details styles

--- frontend/src/app/components/modules/modals/cart/cartModal.tsx
+++ frontend/src/app/components/modules/modals/cart/cartModal.tsx
@@ -16,15 +16,13 @@ export default function CartModal({ toggleModal }: CartModalProps) {
       className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 bottom-0 z-50  justify-center items-center w-full h-full md:inset-0 max-h-full bg-gray-900 bg-opacity-50"
     >
       <div className="relative p-4 w-full max-w-5xl max-h-full">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-              Cart
-            </h3>
+        <div className="relative bg-white rounded-lg shadow">
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 className="text-xl font-semibold text-gray-900">Cart</h3>
             <button
               onClick={toggleModal}
               type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
               data-modal-hide="default-modal"
             >
               <svg
@@ -169,8 +167,8 @@ export default function CartModal({ toggleModal }: CartModalProps) {
                   </tbody>
                 </table>
               </div>
-              <div className="flex items-center p-4 md:p-5 border-gray-200 rounded-b dark:border-gray-600">
-                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              <div className="flex items-center p-4 md:p-5 border-gray-200 rounded-b">
+                <h3 className="text-xl font-semibold text-gray-900">
                   Total: $
                   {items
                     .reduce(
--- frontend/src/app/components/modules/modals/productDetails/productDetailsModal.tsx
+++ frontend/src/app/components/modules/modals/productDetails/productDetailsModal.tsx
@@ -31,16 +31,16 @@ export default function ProductDetailsModal({
       className="hidden fixed top-0 right-0 left-0 bottom-0 z-50 justify-center items-center w-full h-full bg-gray-900 bg-opacity-50"
     >
       <div className="relative p-4 w-full max-w-2xl max-h-full">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+        <div className="relative bg-white rounded-lg shadow">
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+            <h3 className="text-xl font-semibold text-gray-900">
               Product Details
             </h3>
             <button
               data-testid="close-modal"
               onClick={toggleModal}
               type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
               data-modal-hide="product-details-modal"
             >
               <svg
